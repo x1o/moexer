f84a11af7151095297a6fb3ef87b42e608a81b92
Dmitry Zotikov <dmitry.zotikov@devexperts.com>
2022-07-22 14:43:08 +0300

    Implement following cursor; add debug output; better response field type parsing

--- R/iss.R
+++ R/iss.R
@@ -6,6 +6,9 @@
 #' @param params A HTTP GET query parameters string passed as a `list`.
 #' @param iss_base_url The base ISS URL.
 #' @param debug_output Print REST URLs as they are queried.
+#' @param follow_cursor If `TRUE`, iterative queries will be issued to fetch all
+#'   section pages as indicated by `<section>.cursor`; the cursor section
+#'   itself will be removed from the response.
 #'
 #' @return A list where every element is section as returned by ISS.  The
 #'   section content is parsed as a `tibble`.
@@ -23,8 +26,10 @@ query_iss <- function(
         rest_path,
         params = list(),
         iss_base_url = getOption('moexer.iss.base_url'),
-        debug_output = getOption('moexer.debug')
+        debug_output = getOption('moexer.debug'),
+        follow_cursor = TRUE
     ) {
+    iss_query_url <- glue('{iss_base_url}/{rest_path}.json')
     iss_param_str <-
         params %>%
         compact %>%
@@ -32,7 +37,6 @@ query_iss <- function(
             glue('{name}={val}')
         }) %>%
         paste0(collapse = '&')
-    iss_query_url <- glue('{iss_base_url}/{rest_path}.json')
     if (str_length(iss_param_str) > 0) {
         iss_query_url <- glue('{iss_query_url}?{iss_param_str}')
     }
@@ -42,6 +46,38 @@ query_iss <- function(
     resp_parsed <-
         httr::GET(iss_query_url) %>%
         parse_iss_json_reponse()
+    if (follow_cursor) {
+        for (section_name in names(resp_parsed)) {
+            if (str_detect(section_name, '.*\\.cursor')) {
+                cursor_section <- resp_parsed[[section_name]]
+                source_section_name <- str_remove(section_name, '\\.cursor')
+                source_section <- resp_parsed[[source_section_name]]
+                n_pages <- ceiling(cursor_section$TOTAL / cursor_section$PAGESIZE) - 1
+                remaining_section_pages <-
+                    seq_len(n_pages) %>%
+                    map_dfr(function(page_n) {
+                        query_iss(
+                            rest_path,
+                            params = c(
+                                params,
+                                list(
+                                    iss.only = source_section_name,
+                                    start = page_n * cursor_section$PAGESIZE
+                                )
+                            ),
+                            iss_base_url = iss_base_url,
+                            debug_output = debug_output,
+                            follow_cursor = FALSE
+                        )[[1]]
+                    })
+                resp_parsed[[source_section_name]] <- bind_rows(
+                    source_section,
+                    remaining_section_pages
+                )
+                resp_parsed[[section_name]] <- NULL
+            }
+        }
+    }
 
     return(resp_parsed)
 }
@@ -52,9 +88,7 @@ parse_iss_json_reponse <- function(iss_json_response) {
         iss_json_response %>%
         httr::content('text') %>%
         jsonlite::fromJSON(simplifyVector = TRUE, simplifyMatrix = FALSE) %>%
-        map(function(iss_response_section) {
-            parse_iss_response_section(iss_response_section)
-        })
+        map(~ parse_iss_response_section(.x))
 
     return(parsed_list)
 }
@@ -62,15 +96,26 @@ parse_iss_json_reponse <- function(iss_json_response) {
 
 parse_iss_response_section <- function(iss_response_section) {
     parse_type <- function(col, col_name) {
+        data_type <- iss_response_section$metadata[[col_name]]$type
         parse_fn <-
             switch(
-                iss_response_section$metadata[[col_name]]$type,
+                data_type,
                 string = readr::parse_character,
-                int32 = readr::parse_double,
-                int64 = readr::parse_double,
+                # int32 = readr::parse_double,
+                # int64 = readr::parse_double,
+                int32 = function(x) x,
+                int64 = function(x) x,
                 date = readr::parse_date,
                 datetime = readr::parse_datetime,
-                double = readr::parse_double
+                double = readr::parse_double,
+                time = readr::parse_time,
+                undefined = {
+                    cli_alert_warning(glue(
+                        '"{col_name}": Undefined type (parsed as character)'
+                    ))
+                    readr::parse_character
+                },
+                abort(glue('"{col_name}": Unknown type "{data_type}"'))
             )
         return(parse_fn(col))
     }
